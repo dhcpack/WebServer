745f4626243775c6b437b5d63799a5265f7f138c
dhcpack <1928521033@qq.com>
2023-05-03 10:22:01 +0800

    :white_check_mark: log test finished

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 void Thread_push(int id) {
-    for (int i = 1; i <= 50000; i++) {
+    for (int i = 1; i <= 1000; i++) {
         std::cout << id << "--" << i << std::endl;
         Log::instance().write_log(Level::DEBUG, "This is %d by thread %d\n", i, id);
     }
 }
 
 int main() {
-    Log::instance().init();
+    Log::instance().init(INT16_MAX);
     // 新建十个线程执行函数Thread_push, Thread_push中传递的参数分别是1~10
     vector<std::thread> threads;
-    for (int i = 0; i < 50; i++) {
+    for (int i = 1; i <= 1000; i++) {
         threads.emplace_back(Thread_push, i);
     }
 
