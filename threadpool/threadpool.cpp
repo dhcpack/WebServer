f26198bef99e97ed9cf55ed1a03f308a80839186
dhcpack <1928521033@qq.com>
2023-05-03 12:32:53 +0800

    :heavy_plus_sign: finish threadpool

--- threadpool/threadpool.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by ylhappy on 2023/4/25.
-//
-#include "threadpool.h"
--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -5,4 +5,64 @@
 #ifndef WEBSERVER_THREADPOOL_H
 #define WEBSERVER_THREADPOOL_H
 
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+#include <functional>
+
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t threadCount = 8) : pool_(std::make_shared<Pool>()) {
+        for (size_t i = 0; i < threadCount; i++) {
+            // 新建线程，从tasks队列中取任务并执行
+            std::thread([pool = pool_] {
+                std::unique_lock<std::mutex> locker(pool->mtx);
+                while (true) {
+                    if (!pool->tasks.empty()) {
+                        auto task = std::move(pool->tasks.front());
+                        pool->tasks.pop();
+                        locker.unlock();
+                        task();
+                        locker.lock();
+                    } else if (pool->isClosed) break;
+                    else pool->cond.wait(locker);
+                }
+            }).detach();
+        }
+    }
+
+    ThreadPool() = default;
+
+    ThreadPool(ThreadPool &&) = default;
+
+    ~ThreadPool() {
+        if (static_cast<bool>(pool_)) {
+            {
+                std::lock_guard<std::mutex> locker(pool_->mtx);
+                pool_->isClosed = true;
+            }
+            pool_->cond.notify_all();
+        }
+    }
+
+    template<class F>
+    void AddTask(F &&task) {  // 模板函数，右值引用  ==>  万能引用
+        {
+            std::lock_guard<std::mutex> locker(pool_->mtx);
+            pool_->tasks.emplace(std::forward<F>(task));  // 完美转发  保持左值和右值
+        }
+        pool_->cond.notify_one();
+    }
+
+private:
+    struct Pool {
+        std::mutex mtx;
+        std::condition_variable cond;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks;
+    };
+    std::shared_ptr<Pool> pool_;
+};
+
 #endif // WEBSERVER_THREADPOOL_H
