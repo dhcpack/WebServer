ff097d3c3f798c74587b9ddff59029552762b454
dhcpack <1928521033@qq.com>
2023-05-19 22:15:09 +0800

    :rocket: CI/CD

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 [![License: Apache-2.0](https://img.shields.io/github/license/dhcpack/WebServer)](https://img.shields.io/github/license/dhcpack/WebServer)
 
 
+```bash
+  cmake -S . -B build
+  cmake --build build
+```
---
+++ build/CMakeFiles/WebServer.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebServer.dir/buffer/buffer.cpp.o"
+  "CMakeFiles/WebServer.dir/buffer/buffer.cpp.o.d"
+  "CMakeFiles/WebServer.dir/http/httpconnect.cpp.o"
+  "CMakeFiles/WebServer.dir/http/httpconnect.cpp.o.d"
+  "CMakeFiles/WebServer.dir/http/httprequest.cpp.o"
+  "CMakeFiles/WebServer.dir/http/httprequest.cpp.o.d"
+  "CMakeFiles/WebServer.dir/http/httpresponse.cpp.o"
+  "CMakeFiles/WebServer.dir/http/httpresponse.cpp.o.d"
+  "CMakeFiles/WebServer.dir/http/userfunctions.cpp.o"
+  "CMakeFiles/WebServer.dir/http/userfunctions.cpp.o.d"
+  "CMakeFiles/WebServer.dir/log/log.cpp.o"
+  "CMakeFiles/WebServer.dir/log/log.cpp.o.d"
+  "CMakeFiles/WebServer.dir/main.cpp.o"
+  "CMakeFiles/WebServer.dir/main.cpp.o.d"
+  "CMakeFiles/WebServer.dir/mysqlpool/mysqlpool.cpp.o"
+  "CMakeFiles/WebServer.dir/mysqlpool/mysqlpool.cpp.o.d"
+  "CMakeFiles/WebServer.dir/server/epoller.cpp.o"
+  "CMakeFiles/WebServer.dir/server/epoller.cpp.o.d"
+  "CMakeFiles/WebServer.dir/server/webserver.cpp.o"
+  "CMakeFiles/WebServer.dir/server/webserver.cpp.o.d"
+  "CMakeFiles/WebServer.dir/timer/heaptimer.cpp.o"
+  "CMakeFiles/WebServer.dir/timer/heaptimer.cpp.o.d"
+  "WebServer"
+  "WebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
